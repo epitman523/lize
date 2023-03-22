fd20ba7c6fd354264b84d706defe144af9dcec6e
Elizabeth Emig <elizabethemig@Elizabeths-MBP.lan>
2023-03-22 18:16:15 -0500

    Fixed nav scroll issue. Added intro text and styles. Added skills section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector('.header__menu');
 const btn = menu.querySelector('#header__btn--toggle');
+const body = document.querySelector('#body');
 btn.addEventListener('click', evt => {
 	menu.classList.toggle('active');
+	body.classList.toggle('noScroll');
 })
