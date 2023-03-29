b43aee96689f06ace73e7cf7065d1119f6c07e4c
Elizabeth Emig <elizabethemig@Elizabeths-MBP.lan>
2023-03-29 17:51:39 -0500

    Completed work layout 1 structure and styles

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('.header__menu');
 const btn = menu.querySelector('#header__btn--toggle');
-const body = document.querySelector('#body');
+const body = document.querySelector('.body');
 btn.addEventListener('click', evt => {
 	menu.classList.toggle('active');
 	body.classList.toggle('noScroll');
